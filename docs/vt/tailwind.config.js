ee2c0d0e76557a4c9a199c95bd89004b36869d39
Amirjs97 <amirjs.prog@gmail.com>
2023-09-06 19:34:38 +0330

    project docs

--- docs/vt/tailwind.config.js
+++ docs/vt/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}"
+    "%PUBLIC_URL%/./index.html",
+    "%PUBLIC_URL%./src/**/*.{js,jsx}"
   ],
   darkMode: "class",
   theme: {
