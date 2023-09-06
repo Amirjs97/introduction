fbb24494661692b9fbc1f59e8642632af4664c79
Amirjs97 <amirjs.prog@gmail.com>
2023-09-06 22:42:01 +0330

    commiting project intro 17

--- main files/src/components/Contactme.jsx
+++ main files/src/components/Contactme.jsx
@@ -15,7 +15,7 @@ const Contactme = () => {
     return (
         <>
             <AnimatePresence>
-                <div className="flex flex-col w-[90%] my-20 mx-8 min-h-[10rem] justify-center items-center gap-4 p-2 rounded-lg" >
+                <div className="flex flex-col w-[50rem] my-20 mx-8 min-h-[10rem] justify-center items-center gap-4 p-2 rounded-lg" >
                     <div className="w-fit h-full gap-4 m-3 bg-blue-200 p-4 rounded-lg border-2 border-blue-800 shadow-xl shadow-blue-900">
                         <motion.div variants={info} animate="hover" className="flex flex-row justify-start items-center gap-3 p-4 m-2" dir="ltr">
                             <i className="fa-solid fa-at fa-2xl text-slate-700 animate-pulse"></i>
